6cb9e76168dc777ef888666922a36077dd88d624
[Fabian Lasso] <[fabianstivenlasso@gmail.com]>
2023-01-25 17:29:41 -0500

    vite.config added

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+//archivo de configuracion
+//podemos añadir plugins
+// se crea e lbuild segun nuestra configuracion en  este archivo
+
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    base: '/js-vite-blackjack/',
+    plugins: []
+});
